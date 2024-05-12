9f7251b18ca43a94db8c5c7a63710b9f387a600c
Mark Llobrera <mark@dirtystylus.com>
2024-05-11 22:04:31 -0400

    image config with local size derivatives

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,7 @@ import pluginRss from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import path from "path";
 
 import pluginFilters from "./_config/filters.js";
 
@@ -62,7 +63,11 @@ export default async function(eleventyConfig) {
 			loading: "lazy",
 			decoding: "async",
 			sizes: '(min-width: 45em) 640px,(min-width: 60em) 960px,100vw'
-		}
+		},
+		filenameFormat: function (id, src, width, format) {
+				let filename = path.basename(src, path.extname(src));
+				return `${filename}-${width}.${format}`;
+		},
 	});
 
 	// Filters
