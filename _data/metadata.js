956ca45da8d84829c7233987bcbfa4014de420d9
Mark Llobrera <mark.llobrera@upstatement.com>
2026-05-13 09:58:52 -0400

    fixing RSS images

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,17 +1,17 @@
 export default {
 	"title": "Mark Llobrera",
-	"url": "https://www.markllobrera.com/",
+	"url": "https://markllobrera.com/",
 	"domain": "markllobrera.com",
 	"language": "en",
 	"description": "This is the online home for Mark Llobrera, housing his blog, reading log, and film log.",
-	"profile_image": "https://www.markllobrera.com/img/meta/mark-llobrera-thumb.jpg",
+	"profile_image": "https://markllobrera.com/img/meta/mark-llobrera-thumb.jpg",
 	"feed": {
 		"subtitle": "This is the online home for Mark Llobrera, housing his blog and reading log.",
 		"filename": "feed.xml",
 		"path": "/feed/feed.xml",
-		"url": "https://www.markllobrera.com/feed/feed.xml",
-		"id": "https://www.markllobrera.com/",
-		"img_base_url": "https://www.markllobrera.com",
+		"url": "https://markllobrera.com/feed/feed.xml",
+		"id": "https://markllobrera.com/",
+		"img_base_url": "https://markllobrera.com",
 	},
 	"author": {
 		"name": "Mark Llobrera",
